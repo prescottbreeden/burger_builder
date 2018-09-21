a7de930bd53bbad359b56696e6394827fd5770b0
prescottbreeden <prescott.breeden@gmail.com>
2018-09-21 03:08:04 -0700

    added ingredients to state and feeding state into burger components to generate hardcoded burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />;
+			});
+		})
+
 	return (
 		<div className="burger">
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+				{transformedIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
